8bd7b0e080ab01ce037d689701ea9e771b56644d
CristianLavin <cristian.lavin02@gmail.com>
2026-04-23 18:36:04 -0400

    fix7

--- src/App.jsx
+++ src/App.jsx
@@ -71,7 +71,7 @@ function App() {
       </section>
 
       {/* About Section */}
-      <section id="about" className="py-20 bg-white">
+      <section id="#about" className="py-20 bg-white">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="lg:text-center mb-12">
             <h2 className="text-3xl font-extrabold text-gray-900 sm:text-4xl">
