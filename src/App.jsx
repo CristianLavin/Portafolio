f212860e3f481f1d8b00f29c1d9c30b290ad75a9
CristianLavin <cristian.lavin02@gmail.com>
2026-04-23 18:25:01 -0400

    fix4

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex justify-between items-center h-16">
             <div className="flex-shrink-0 flex items-center">
-              <span className="font-bold text-xl text-indigo-600">Portafolio</span>
+              <a href="www.cristian-lavin.dev" className="font-bold text-xl text-indigo-600">Portafolio</a>
             </div>
             <div className="flex items-center space-x-4">
               <div className="hidden md:flex space-x-8">
